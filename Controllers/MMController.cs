f8e033a87293cb92f61785a602e47d5a75260e6e
Osama Hasan <osama.hasan@bas.com.bh>
2023-10-13 18:50:10 +0300

    Add project files.

---
+++ Controllers/MMController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SAP_S4_Hana_API.Controllers
+{
+    public class MMController : ApiController
+    {
+    }
+}
